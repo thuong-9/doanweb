ef23cd53377693233e1cff082a77c384fbef4054
Trang pham <trangpd204@gmail.com>
2025-12-23 00:12:26 +0700

    trang

--- obj/Debug/net8.0/EasyCode.AssemblyInfo.cs
+++ obj/Debug/net8.0/EasyCode.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EasyCode")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+da7ff91372cdc98136e10ed175978861450465ba")]
 [assembly: System.Reflection.AssemblyProductAttribute("EasyCode")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EasyCode")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
